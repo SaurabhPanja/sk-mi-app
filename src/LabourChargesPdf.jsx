2b40dd7c520d8e7a8e7b02f687b83439c862335d
Saurabh Panja <saurabh.panja@claritas.com>
2024-05-05 01:32:11 +0530

    labour charges done

--- src/LabourChargesPdf.jsx
+++ src/LabourChargesPdf.jsx
@@ -1,25 +1,31 @@
 import { Document, Page, Text, View, StyleSheet, Font } from '@react-pdf/renderer';
 
-Font.register(
-  { family: 'opensans', src: '/fonts/opensans.ttf' })
+Font.register({
+  family: 'opensans',
+  src: '/fonts/opensans.ttf'
+});
 
 const styles = StyleSheet.create({
   page: {
     margin: '5px',
     fontSize: '12px',
-    // marginRight: '12px',
-    // width: '100px',
     fontFamily: 'opensans'
   },
   header: {
     backgroundColor: "#00BFFE",
     fontWeight: 'bold',
-    color: '#fff'
+    color: '#fff',
+    height: "60px",
+    // fontSize: "36px",
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '0 20px'
   },
   title: {
-    fontSize: '16px',
+    fontSize: '24px',
     fontWeight: 'bold',
-    // marginBottom: 5,
     color: '#fff'
   },
   companyInfo: {
@@ -37,7 +43,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRightWidth: 0,
     borderBottomWidth: 0,
-
   },
   tableRow: {
     flexDirection: 'row',
@@ -47,8 +52,7 @@ const styles = StyleSheet.create({
     borderColor: '#000',
     borderBottomWidth: 1,
     borderRightWidth: 1,
-    // padding: 2,
-    // width: '20%'
+    padding: 2,
   },
   tableHeader: {
     fontWeight: 'bold',
@@ -61,33 +65,46 @@ const styles = StyleSheet.create({
 });
 
 const InvoiceHeader = () => (
-  <View style={[styles.tableRow, styles.header]}>
-      <Text style={{ width: '33%', textAlign: 'left'}}>Mob no: 9898832796</Text>
-      <Text style={{ width: '33%', textAlign: 'center', ...styles.title}}>SK Maidul Islam</Text>
-      <Text style={{ width: '33%', textAlign:'right'}}>Nadiad &nbsp;&nbsp;</Text>
+  <View style={styles.header}>
+    <Text style={{ width: '33%', textAlign: 'left' }}>Mob no: 9898832796</Text>
+    <Text style={styles.title}>SK Maidul Islam</Text>
+    <Text style={{ width: '33%', textAlign: 'right' }}>Nadiad &nbsp;&nbsp;</Text>
   </View>
 );
 
-export default ({ labourCharges }) => (
-  <Document>
-    <Page style={styles.page}>
-      <InvoiceHeader />
-      <View style={styles.table}>
-        <View style={[styles.tableRow, styles.tableHeader]}>
-          <Text style={{ width: '5%', ...styles.tableCol }}>&nbsp; #</Text>
-          <Text style={{ width: '50%', ...styles.tableCol }}>&nbsp; Particular</Text>
-          <Text style={{ width: '20%', ...styles.tableCol }}>&nbsp; Sqf/R.feet</Text>
-          <Text style={{ width: '25%', ...styles.tableCol }}>&nbsp; Rate</Text>
-        </View>
-        {labourCharges && labourCharges.map((item, index) => (
-          <View style={styles.tableRow} key={index}>
-            <Text style={{ width: '5%', ...styles.tableCol }}>&nbsp;{index + 1}</Text>
-            <Text style={{ width: '50%', ...styles.tableCol }}>&nbsp;{item.particular}</Text>
-            <Text style={{ width: '20%', ...styles.tableCol }}>&nbsp;{item.unit}</Text>
-            <Text style={{ width: '25%', ...styles.tableCol }}>&nbsp;{item.rate}</Text>
+export default ({ labourCharges }) => {
+  const itemsPerPage = 32;
+  const pages = [];
+  let currentPage = [];
+
+  labourCharges.forEach((item, index) => {
+    currentPage.push(item);
+
+    if ((index + 1) % itemsPerPage === 0 || index === labourCharges.length - 1) {
+      pages.push(
+        <Page style={styles.page} key={`page-${pages.length}`}>
+          <InvoiceHeader />
+          <View style={styles.table}>
+            <View style={[styles.tableRow, styles.tableHeader]}>
+              <Text style={{ width: '5%', ...styles.tableCol }}>&nbsp; #</Text>
+              <Text style={{ width: '50%', ...styles.tableCol }}>&nbsp; Particular</Text>
+              <Text style={{ width: '20%', ...styles.tableCol }}>&nbsp; Sqf/R.feet</Text>
+              <Text style={{ width: '25%', ...styles.tableCol }}>&nbsp; Rate</Text>
+            </View>
+            {currentPage.map((item, idx) => (
+              <View style={styles.tableRow} key={`item-${idx}`}>
+                <Text style={{ width: '5%', ...styles.tableCol }}>&nbsp;{idx + 1}</Text>
+                <Text style={{ width: '50%', ...styles.tableCol }}>&nbsp;{item.particular}</Text>
+                <Text style={{ width: '20%', ...styles.tableCol }}>&nbsp;{item.unit}</Text>
+                <Text style={{ width: '25%', ...styles.tableCol }}>&nbsp;{item.rate}</Text>
+              </View>
+            ))}
           </View>
-        ))}
-      </View>
-    </Page>
-  </Document>
-);
+        </Page>
+      );
+      currentPage = [];
+    }
+  });
+
+  return <Document>{pages}</Document>;
+};
